37a2f59cebd99430390665c746f0e94517f7e7d8
Elliott Ubelhor <elliottubelhor@MacBook-Air.local>
2023-11-07 19:24:32 -0600

    final commit

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -36,7 +36,7 @@ export default class CustomModal extends Component {
 
     return (
       <Modal isOpen={true} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
+        <ModalHeader toggle={toggle}>New Task</ModalHeader>
         <ModalBody>
           <Form>
             <FormGroup>
@@ -47,7 +47,7 @@ export default class CustomModal extends Component {
                 name="title"
                 value={this.state.activeItem.title}
                 onChange={this.handleChange}
-                placeholder="Enter Todo Title"
+                placeholder="Enter Your Task Here"
               />
             </FormGroup>
             <FormGroup>
@@ -58,7 +58,7 @@ export default class CustomModal extends Component {
                 name="description"
                 value={this.state.activeItem.description}
                 onChange={this.handleChange}
-                placeholder="Enter Todo description"
+                placeholder="Provide a brief description..."
               />
             </FormGroup>
             <FormGroup check>
